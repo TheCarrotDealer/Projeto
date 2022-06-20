eaf78bec3db6794d9be9c9272e808c4022f7fae3
TheCarrotDealer <106707263+TheCarrotDealer@users.noreply.github.com>
2022-06-20 03:38:31 +0100

    20/06
    good good

--- projeto.py
+++ projeto.py
@@ -5,24 +5,17 @@ from implementacao1 import *
 from implementacao2 import *
 from implementacao3 import *
 from implementacao4 import *
+
 def menu():
+
     win1 = GraphWin("Projeto Ai Menu", 600, 600)
     win1.setCoords(0, 0, 600, 600)
-    point1 = Point(30,30)
-    point2 = Point(170,70)
-    point3 = Point( 200,30)
-    point4 = Point(350,70)
-    point5 = Point( 380,30)
-    point6 = Point(520,70)
-    point7 = Point( 230,130)
-    point8 = Point(370,170)
-    point9 = Point(250,500)
-    point10 = Point(350,550)
-    quitbutton = butão(win1,point9,point10,"quit")
-    implementacao1 = butão(win1,point1,point2,"implementação 1")
-    implementacao2 = butão(win1,point3,point4,"implementação 2")
-    implementacao3 = butão(win1,point5,point6,"implementação 3")
-    implementacao4 = butão(win1,point7,point8,"implementação 4")
+   
+    quitbutton = butão(win1,Point(250,500),Point(350,550),"quit")
+    implementacao1 = butão(win1,Point(30,30),Point(170,70),"implementação 1")
+    implementacao2 = butão(win1,Point( 200,30),Point(350,70),"implementação 2")
+    implementacao3 = butão(win1,Point( 380,30),Point(520,70),"implementação 3")
+    implementacao4 = butão(win1,Point( 230,130),Point(370,170),"implementação 4")
     while True:
         click = win1.getMouse()
         if implementacao1.clicked(click):
