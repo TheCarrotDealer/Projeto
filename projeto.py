7b73be84f444638aa13848dac31a11fd5b9ad302
TheCarrotDealer <106707263+TheCarrotDealer@users.noreply.github.com>
2022-06-21 01:22:04 +0100

    21/06
    hmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm

--- projeto.py
+++ projeto.py
@@ -1,11 +1,19 @@
 from graphics import * 
 from classes import *
+from random import *
 from outrasclasses import *
 from implementacao1 import *
 from implementacao2 import *
 from implementacao3 import *
 from implementacao4 import *
-
+def funny():
+    while True:
+        update(5)
+        win3 = GraphWin("ha", 370, 184)
+        win3.setCoords(0, 0, 370, 184)
+        ha = Image(Point(185,92),"ha.png")
+        ha.draw(win3)
+    
 def menu():
 
     win1 = GraphWin("Projeto Ai Menu", 1000, 550)
@@ -15,6 +23,12 @@ def menu():
     menubackround.draw(win1)
     title = Image(Point(200,500),"title.png")
     title.draw(win1)
+
+    buttoneasteregg = butão(win1,Point(230,480),Point(250,505)," ")
+    red = Rectangle(Point(230,480),Point(250,505))
+    red.setOutline("red")
+    red.setFill("red")
+    red.draw(win1)
    
     quitbutton = butão(win1,Point(870,30),Point(970,70),"quit")
     implementacao1 = butão(win1,Point(830,475),Point(970,525),"implementação 1")
@@ -23,6 +37,10 @@ def menu():
     implementacao4 = butão(win1,Point( 830,250),Point(970,300),"implementação 4")
     while True:
         click = win1.getMouse()
+        if buttoneasteregg.clicked(click):
+            a= Text(Point(randrange(10,990),randrange(540)),"apple")
+            a.draw(win1)
+            funny()
         if implementacao1.clicked(click):
             win1.close()
             implementação1()
